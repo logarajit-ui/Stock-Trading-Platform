c5fc89f90cf80323c396411db8619505c81a992b
logarajit-ui <logarajit@gmail.com>
2026-07-16 20:43:07 +0530

    Add files via upload

---
+++ Stock.java
@@ -0,0 +1,18 @@
+public class Stock {
+
+    private String stockName;
+    private double price;
+
+    public Stock(String stockName, double price) {
+        this.stockName = stockName;
+        this.price = price;
+    }
+
+    public String getStockName() {
+        return stockName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
